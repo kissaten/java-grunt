4e51860529c854fe099c3a012c29bc24487804ce
Joe Kutner <jpkutner@gmail.com>
2014-12-11 09:21:12 -0600

    improve grunt setup

--- src/main/resources/public/js/home.js
+++ src/main/resources/public/js/home.js
@@ -1,7 +1,3 @@
 $(function () {
-  $('div.container').click(function (e) {
-    alert(e.target.nodeName);
-    alert(e.currentTarget.nodeName);
-  });
-
+  $('body p').append(" and Javascript!");
 });
